49d349d0b04cdc1d94ab27b3a88194424377fc77
Chris Nosowsky <chrisnosowsky@gmail.com>
2024-01-20 18:57:32 -0500

    Function to save results to CSV; Update plot;

--- matrix_multi.cpp
+++ matrix_multi.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 #include <stdexcept>
 #include <random>
+#include <fstream>
+// #include <iomanip>
 extern "C" {
 #include "get_walltime.c"
 }
 
-#define N 100
-#define REPEAT 1000
+#define FILENAME "data.csv"
 
+const size_t N = 1024;
+const size_t REPEAT = 20;
 
-
+// #define PRECISION 3
+// #define GROUP_MEMBERS {"Chris", "Onur", "Melina", "Hunter"}
 
 struct ComputeComponent{
     std::string name;
@@ -18,8 +22,6 @@ struct ComputeComponent{
     float clock_speed_ghz;
     int num_fpus;
 };
-
-
 
 double lower_bound = 0.0f;
 double upper_bound = 10000.0f;
@@ -76,14 +78,65 @@ double CalculateMFLOPS(double total_time){
 }
 
 double CalculateTheoreticalPeakPerformance(const ComputeComponent& comp){
-
     double peak_perf = comp.num_cores * comp.num_fpus * comp.clock_speed_ghz * 1000.0f;
     return peak_perf;
-
 }
 
-// f(v1, v2) -> csv
-// 
+
+void SaveResultsToCSV(const std::string& filename, const std::string& peak_performance, const std::string& achieved_performance){
+    if(filename.substr(filename.size() - 4) != ".csv"){
+        std::cout << "Error: Invalid file format. Please specify a CSV file." << std::endl;
+        return;
+    }
+
+    std::ifstream infile(filename, std::ios::in | std::ios::out);
+
+    if(!infile){
+        std::ofstream outfile(filename);
+        outfile.close();
+    }
+
+    std::string line;
+    std::string matrix_size_str = std::to_string(N);
+    std::streampos found_pos;
+    std::streampos line_lengths = 0;
+    bool matrix_size_found = false;
+
+    while (std::getline(infile, line)) {
+        size_t position_in_line = line.find(matrix_size_str);
+        if (position_in_line != std::string::npos) {
+            std::streampos stream_position = static_cast<std::streampos>(position_in_line);
+            found_pos = line_lengths + stream_position;
+            matrix_size_found = true;
+            break;
+        } else {
+            size_t line_length = line.length();
+            line_lengths += static_cast<std::streampos>(line_length + 1);
+        }
+    }
+
+    std::ofstream outfile(filename, std::ios::in | std::ios::out);
+    std::string header_line = "matrix_size,peak_performance,achieved_performance";
+
+    outfile.seekp(0, std::ios::end);
+
+    if(outfile.tellp() == 0){
+        outfile << header_line << std::endl;
+    }
+
+    outfile.seekp(0, std::ios::beg);
+
+    if(matrix_size_found) {
+        outfile.seekp(found_pos);
+        outfile << N << "," << peak_performance << "," << achieved_performance; 
+    } else {
+        outfile.seekp(0, std::ios::end);
+        outfile << N << "," << peak_performance << "," << achieved_performance << std::endl;
+    }
+
+    infile.close();
+    outfile.close();
+}
 
 
 double GenerateRandomNumber(){
@@ -91,9 +144,24 @@ double GenerateRandomNumber(){
     return a_random_double;
 }
 
-int main() {
-    
+int main(int argc, char* argv[]) {
+    // int N = 0;
 
+    // if (argc > 1) {
+    //     // convert argument to integer
+    //     int arg = std::stoi(argv[1]);
+        
+    //     // check if it's a number that's more than 0 and less than 10000
+    //     if (arg > 0 && arg < 10000) {
+    //         N = arg;
+    //     } else {
+    //         std::cout << "Invalid argument. Please enter a number between 1 and 10000." << std::endl;
+    //         return;
+    //     }
+    // } else {
+    //     std::cout << "No argument provided. Defaulting to N=100." << std::endl;
+    //     N = 100;
+    // }
     std::vector<std::vector<double>> mat1(N, std::vector<double>(N));
     std::vector<std::vector<double>> mat2(N, std::vector<double>(N));
     
@@ -103,14 +171,6 @@ int main() {
 
 
     printf("Starting the program with %dx%d matrices\n", N, N);
-
-    // mat1 = {{1, 2},
-    //         {3, 4}};
-    
-
-    // mat2 = {{1, 2},
-    //         {3, 4}};
-    
 
     for(size_t j=0; j < mat1[0].size(); j++){
         for(size_t k=0; k < mat1[0].size(); k++){
@@ -135,14 +195,15 @@ int main() {
         total_time += elapsed_time;
     }
 
-    printf("Total Seconds: %f\n", total_time);  // Uncomment future
-    printf("Average Seconds: %f\n", total_time / REPEAT);  // Uncomment future
+    printf("Total Seconds: %f\n", total_time);  // Remove future
+    printf("Average Seconds: %f\n", total_time / REPEAT);  // Remove future
 
     double mflops = CalculateMFLOPS(total_time);
-    ComputeComponent cc = {"Chris", 4, 3.0f, 1};
-    const auto& peak_perf_chris = CalculateTheoreticalPeakPerformance(cc);
 
-    printf("Theoretical Performance [%s] is: %f MFLOPS\n", cc.name.c_str(), peak_perf_chris); 
+
+    ComputeComponent cc = {"Chris", 4, 3.0f, 1};
+    const auto& peak_perf = CalculateTheoreticalPeakPerformance(cc);
+    printf("Theoretical Performance [%s] is: %f MFLOPS\n", cc.name.c_str(), peak_perf); 
 
 
     // PrintMatrix(res);
@@ -161,8 +222,8 @@ int main() {
     // Part 2 notes:
     // 1.
 
-    printf("Total time took with %d repeats: %f Mflop/s\n", REPEAT, mflops);
-
-    printf("Closing on the peak performance with %f\n", mflops / peak_perf_chris);
-    
+    printf("Total time took with %d repeats: %f MFLOPS\n", REPEAT, mflops);
+    printf("Writing results to %s\n", FILENAME);
+    SaveResultsToCSV(FILENAME, std::to_string(peak_perf * 1e-3), std::to_string(mflops * 1e-3));
+    printf("Finished writing results to %s\nExiting.", FILENAME);
 }
--- plot_performance.py
+++ plot_performance.py
@@ -3,28 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Step: On CPP side, export information to CSV
-# TODO: Save as CSV then Plot results
-# Chris will work on
-
-
-# data.csv looks like this:
-# 
-
 FILENAME = "data.csv"
 
 def plot_performance_results(filename):
     df = pd.read_csv(filename)
-    n_values = df['matrix_size'].astype(np.float32)
+    n_values = df['matrix_size'].astype(np.float32).sort_values()
     peak_performance = df['peak_performance'].astype(np.float32)
     achieved_performance = df['achieved_performance'].astype(np.float32)
-    plt.plot(n_values, achieved_performance, label="Achieved Peformance", color="red")
-    plt.axhline(y=peak_performance[0], label="Peak Performance", color="blue")
+
+    plt.scatter(n_values, achieved_performance, label="Achieved Performance", color="red", marker="o")
+    plt.plot(n_values, achieved_performance, color='red', alpha=0.5) 
+    plt.hlines(y=peak_performance[0], xmin=n_values.min(), xmax=n_values.max(), label="Peak Performance", color="blue", linestyles='dashed')
+    
+
     plt.xlabel("Matrix Size (N)")
     plt.ylabel("GFLOPS")
     plt.title("Peak vs. Achieved Performance")
-    plt.legend()
-    plt.savefig('test.png')
+    plt.legend(loc="center right", fancybox=True, shadow=True)
+    plt.savefig('peak_vs_achieved_performance.png', bbox_inches='tight')
     plt.show()
 
 
